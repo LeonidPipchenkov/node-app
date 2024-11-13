688b7ff168a67b4c9b15aa5aba1341bd87e692e2
Leonid Pipchenkov <t70a80@gmail.com>
2024-11-13 12:48:37 +0300

    Add a little timeout

--- index.mjs
+++ index.mjs
@@ -2,5 +2,7 @@ import fs from 'fs'
 
 fs.appendFile('my-file.txt', 'Файл создан Node.js', (err) => {
     if (err) throw err
-    console.log('Файл сохранен!')
+    console.log('Файл сохранён успешно!')
 })
+
+setTimeout(() => console.log('Конец программы!'), 30000)
